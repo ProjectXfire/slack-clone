c334f2472953ca28b0bcbb4052dca9c2d6310078
gvbdeveloper <gabriel.vargas.bravo@gmail.com>
2024-10-28 16:27:05 -0500

    add: member profile panel

--- app/(workspaces)/_components/resizable-content/ResizableContent.tsx
+++ app/(workspaces)/_components/resizable-content/ResizableContent.tsx
@@ -1,21 +1,22 @@
 "use client";
 
-import styles from "./ResizableContent.module.css";
+import { useConversationId } from "../../_stores";
 import { usePanel } from "../../_hooks";
+import styles from "./ResizableContent.module.css";
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/shared/components";
 import SidebarContent from "../sidebar/SidebarContent";
 import Thread from "../thread/Thread";
-import { useConversationId } from "../../_stores";
+import Profile from "../profile/Profile";
 
 interface Props {
   children: React.ReactNode;
 }
 
 function ResizableContent({ children }: Props): JSX.Element {
-  const { parentMessageId, onClose } = usePanel();
+  const { parentMessageId, profileMemberId, onClose } = usePanel();
   const [conversationId, setConversationId] = useConversationId();
 
-  const showPanel = !!parentMessageId;
+  const showPanel = !!parentMessageId || !!profileMemberId;
 
   const closeThread = () => {
     onClose();
@@ -35,11 +36,14 @@ function ResizableContent({ children }: Props): JSX.Element {
         <>
           <ResizableHandle withHandle />
           <ResizablePanel id="thread-panel" order={3} defaultSize={20} minSize={20}>
-            <Thread
-              messageId={parentMessageId}
-              conversationId={conversationId}
-              onClose={closeThread}
-            />
+            {parentMessageId && (
+              <Thread
+                messageId={parentMessageId}
+                conversationId={conversationId}
+                onClose={closeThread}
+              />
+            )}
+            {profileMemberId && <Profile memberId={profileMemberId} onClose={closeThread} />}
           </ResizablePanel>
         </>
       )}
